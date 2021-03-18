a35c73ef8d5af484ae50918ec0fac6c45e21fe2d
Rebecca Weirich <b.weirich@yahoo.com>
2021-03-17 20:33:44 -0500

    Adding the election audit code

--- PyPoll.py
+++ PyPoll.py
@@ -15,6 +15,20 @@ file_to_load = os.path.join('Resources', 'election_results.csv')
 # Create a filename variable to a file.
 file_to_save = os.path.join('analysis', 'election_analysis.txt')
 
+# Initialize total vote counter
+total_votes = 0
+
+# Initialize candidate list
+candidate_options = []
+
+# Initalize dictionary for candidate votes
+candidate_votes = {}
+
+# Winning candidate tracker
+winning_candidate = ''
+winning_count = 0
+winning_percentage = 0
+
 # Open the election results and read the file
 with open(file_to_load) as election_data:
 
@@ -22,8 +36,42 @@ with open(file_to_load) as election_data:
 
     # Print the header row
     headers = next(file_reader)
-    print(headers)
     
+    # Print each row in csv file
+    for row in file_reader:
+        # add to total vote count
+        total_votes += 1
+        # find candidate name for each row
+        candidate_name = row[2]
+        # add candidate name to list
+        if candidate_name not in candidate_options:
+            candidate_options.append(candidate_name)
+            # begin tracking votes for candidate
+            candidate_votes[candidate_name] = 0
+        candidate_votes[candidate_name] += 1
+
+# Calculate percentage of votes for candidates
+for candidate in candidate_votes:
+    votes = candidate_votes[candidate]
+    vote_percentage = votes / total_votes * 100
+    print(f'{candidate}: {vote_percentage: .1f}% ({votes:,})\n')
+    
+    # Find winning candidate
+    if (votes > winning_count) and (vote_percentage > winning_percentage):
+        winning_count = votes
+        winning_percentage = vote_percentage
+        winning_candidate = candidate
+    
+# Print winning information
+winning_candidate_summary = (
+    f"-------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"--------------------------\n"
+)
+print(winning_candidate_summary)
+
 # Open file as text file
 with open(file_to_save, 'w') as txt_file:
     txt_file.write('Counties in the Election\n_ _ _ _ _ _ _ _ _ _ _ _ _ _ _\nArapahoe\nDenver\nJefferson')
